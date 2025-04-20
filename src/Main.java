c1caba049a19f940f0b2dab2b44ec985d5c5c91b
aminazad079 <102756061+aminazad079@users.noreply.github.com>
2025-04-20 19:21:01 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             // ✅ ONLY CHANGE THIS ONE LINE PER TEST CASE!
-            String testFolder = "testcases/7-unschedulable";
+            String testFolder = "../testcases/7-unschedulable";
 
             // Dynamically construct paths
             String taskFile = testFolder + "/tasks.csv";
